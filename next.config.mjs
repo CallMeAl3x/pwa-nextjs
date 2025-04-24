4d4501f523d8dfc256db9bc502fabd3fbd07188b
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2025-04-24 00:40:20 -0400

    test and try

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,8 @@
-// @ts-check
-import withSerwistInit from "@serwist/next";
-
-// You may want to use a more robust revision to cache
-// files more efficiently.
-// A viable option is `git rev-parse HEAD`.
-const revision = crypto.randomUUID();
+import withSerwistInit from '@serwist/next';
 
 const withSerwist = withSerwistInit({
-  cacheOnNavigation: true,
-  swSrc: "app/sw.ts",
-  swDest: "public/sw.js",
-  additionalPrecacheEntries: [{ url: "/~offline", revision }],
+  swSrc: 'src/app/sw.ts',
+  swDest: 'public/sw.js',
 });
 
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-export default withSerwist(nextConfig);
+export default withSerwist({});
