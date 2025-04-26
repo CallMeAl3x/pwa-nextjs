ea1af3a9b97c9cd00921ee82b8058d121cbc9cf8
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2025-04-26 14:52:59 -0400

    downgrade nextjs

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 
-import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
-
-const font = Poppins({
-  subsets: ["latin"],
-  weight: ["600"]
-});
 
 interface HeaderProps {
   label: string;
@@ -15,7 +9,7 @@ interface HeaderProps {
 const Header = ({ label }: HeaderProps) => {
   return (
     <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-      <h1 className={cn("text-3xl font-semibold", font.className)}>Auth</h1>
+      <h1 className={cn("text-3xl font-semibold")}>Auth</h1>
       <p className="text-muted-foreground">{label}</p>
     </div>
   );
